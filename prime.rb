9216735046f391f22ca097061a0754fa87636305
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-05 03:17:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(num)
   if num < 2
     return false
   end
-  test_set = (2..num).to_a
+  test_set = (2...num).to_a
   test_set.each do |test_num|
     if test_num % each == 0
       return false
